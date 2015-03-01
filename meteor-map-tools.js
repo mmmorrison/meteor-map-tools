9cda5fc29ccd161f4394d2ee91b4d9e294a78ece
Yago Ferrer <yago.ferrer@gmail.com>
2015-03-01 08:49:50 -0800

    Remove spaces

--- meteor-map-tools.js
+++ meteor-map-tools.js
@@ -1,20 +1,13 @@
 if (Meteor.isClient) {
-
-
     Meteor.startup(function() {
-
         var map = new mapTools({
             id: 'map',
             lat: 41.3833,
-            lng: 2.1833,
-            type: 'ROADMAP'
+            lng: 2.1833
         }, function (err, instance) {
             console.log('Map Loaded!', instance);
         });
-
     });
-
-
 }
 
 if (Meteor.isServer) {
