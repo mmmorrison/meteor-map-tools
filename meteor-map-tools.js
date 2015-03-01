5983f6739152b151a0b7daf699dcaa886a4e09c5
Yago Ferrer <yago.ferrer@gmail.com>
2015-03-01 15:40:08 -0800

    Add Marker from Mongo Server Side.

--- meteor-map-tools.js
+++ meteor-map-tools.js
@@ -1,3 +1,9 @@
+
+Markers = new Mongo.Collection("markers");
+
+
+
+
 if (Meteor.isClient) {
     Meteor.startup(function() {
         var map = new mapTools({
@@ -5,13 +11,20 @@ if (Meteor.isClient) {
             lat: 41.3833,
             lng: 2.1833
         }, function (err, instance) {
-            console.log('Map Loaded!', instance);
+            map.addMarker(Markers.find({}).fetch());
         });
     });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+
+      Markers.insert({
+          lat: 41.3833,
+          lng: 2.1833,
+          title: 'Barcelona'
+      });
+
     // code to run on server at startup
   });
 }
