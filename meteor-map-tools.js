0af272a7fe27eaa5ecaf0ed6e19dc01e19b3cb91
Yago Ferrer <yago.ferrer@gmail.com>
2015-03-01 08:42:15 -0800

    Display Map!

--- meteor-map-tools.js
+++ meteor-map-tools.js
@@ -1,17 +1,7 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+    Meteor.startup(function() {
 
         var map = new mapTools({
             id: 'map',
@@ -21,10 +11,8 @@ if (Meteor.isClient) {
         }, function (err, instance) {
             console.log('Map Loaded!', instance);
         });
-    }
-  });
 
-
+    });
 
 
 }
