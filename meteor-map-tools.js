33757be61de88aa2c28442a9107396bbb6e00050
Yago Ferrer <yago.ferrer@gmail.com>
2015-02-28 23:42:18 -0800

    Testing map

--- meteor-map-tools.js
+++ meteor-map-tools.js
@@ -12,8 +12,21 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
+
+        var map = new mapTools({
+            id: 'map',
+            lat: 41.3833,
+            lng: 2.1833,
+            type: 'ROADMAP'
+        }, function (err, instance) {
+            console.log('Map Loaded!', instance);
+        });
     }
   });
+
+
+
+
 }
 
 if (Meteor.isServer) {
