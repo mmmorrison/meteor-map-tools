645a04394c3c1fb5fa1e1e93f99ad46d76e2949d
Yago Ferrer <yago.ferrer@gmail.com>
2015-03-01 16:53:10 -0800

    Testing reactivity

--- meteor-map-tools.js
+++ meteor-map-tools.js
@@ -1,30 +1,54 @@
 
+
 Markers = new Mongo.Collection("markers");
 
-
-
+var map;
 
 if (Meteor.isClient) {
     Meteor.startup(function() {
-        var map = new mapTools({
+        map = new mapTools({
             id: 'map',
             lat: 41.3833,
             lng: 2.1833
         }, function (err, instance) {
-            map.addMarker(Markers.find({}).fetch());
+
+            var addedMarkers = map.addMarker(Markers.find({}).fetch());
+
+            // Save UID for Cross reference
+            var item, x;
+            for (x in addedMarkers) {
+                item = addedMarkers[x];
+                Markers.update({_id: item._id}, {$set: {uid: item.data.uid}});
+            }
+
         });
+    });
+
+    Tracker.autorun(function () {
+       var items = Markers.find({}).fetch();
+
+        // Adds Markers found Mongo but not in the Map
+        var item, x;
+       for (x in items) {
+           var item = items[x];
+           if (item.lat && (!item.uid || !map.markers.all[item.uid])) {
+               map.addMarker(item);
+           }
+       }
     });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
 
-      Markers.insert({
-          lat: 41.3833,
-          lng: 2.1833,
-          title: 'Barcelona'
-      });
+      // Reset for reload testing.
+      Markers.remove({})
 
-    // code to run on server at startup
+      if (Markers.find().count() === 0) {
+          Markers.insert({lat: 41.3833,lng: 2.1833, title: 'Barcelona'});
+      }
+
+      // Use this example to Manually add a record using the console:
+      // Markers.insert({lat: 42.5000, lng: 1.5167, title: 'Andorra'});
   });
 }
